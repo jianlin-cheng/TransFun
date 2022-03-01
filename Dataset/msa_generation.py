65951d378c2805914cc197214c136efd240ae26a
frimpz <boadu203@gmail.com>
2022-02-28 21:32:12 -0600

    Commint

--- Dataset/msa_generation.py
+++ Dataset/msa_generation.py
@@ -1,11 +1,11 @@
 import torch
 import esm
 
+
 # Load ESM-1b model
 model, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
 batch_converter = alphabet.get_batch_converter()
-
-exit()
+model.eval()
 
 # Prepare data (first 2 sequences from ESMStructuralSplitDataset superfamily / 4)
 data = [
@@ -16,23 +16,24 @@ data = [
 ]
 batch_labels, batch_strs, batch_tokens = batch_converter(data)
 
-print(batch_labels)
-exit()
-
 # Extract per-residue representations (on CPU)
 with torch.no_grad():
     results = model(batch_tokens, repr_layers=[33], return_contacts=True)
 token_representations = results["representations"][33]
 
+print(token_representations.shape)
+
 # Generate per-sequence representations via averaging
 # NOTE: token 0 is always a beginning-of-sequence token, so the first residue is token 1.
 sequence_representations = []
 for i, (_, seq) in enumerate(data):
+    print(i)
+    print(seq, len(seq))
+    print(token_representations[i, 1 : len(seq) + 1])
+
     sequence_representations.append(token_representations[i, 1 : len(seq) + 1].mean(0))
 
-# Look at the unsupervised self-attention map contact predictions
-import matplotlib.pyplot as plt
-for (_, seq), attention_contacts in zip(data, results["contacts"]):
-    plt.matshow(attention_contacts[: len(seq), : len(seq)])
-    plt.title(seq)
-    plt.show()
+
+
+
+
