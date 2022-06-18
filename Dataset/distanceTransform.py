b8a3e3b3d253988baf2a8d6cad488b8b4bbb8a61
frimpz <23377139+frimpz@users.noreply.github.com>
2022-06-17 20:46:06 -0500

    Added new Transforms

---
+++ Dataset/distanceTransform.py
@@ -0,0 +1,33 @@
+import torch
+
+from torch_geometric.transforms import  Distance
+
+
+class myDistanceTransform(Distance):
+    r"""
+
+    """
+    def __init__(self, edge_types, norm=True, max_value=None, cat=True):
+        super().__init__(norm, max_value, cat)
+        self.edge_types = edge_types
+
+    def __call__(self, data):
+        for i in self.edge_types:
+            (row, col), pos, pseudo = getattr(data, i[1]), data.pos, getattr(data, i[1]+"_edge_attr", None)
+
+            dist = torch.norm(pos[col] - pos[row], p=2, dim=-1).view(-1, 1)
+
+            if self.norm and dist.numel() > 0:
+                dist = dist / (dist.max() if self.max is None else self.max)
+
+            if pseudo is not None and self.cat:
+                pseudo = pseudo.view(-1, 1) if pseudo.dim() == 1 else pseudo
+                setattr(data, i[1]+"_edge_attr", torch.cat([pseudo, dist.type_as(pseudo)], dim=-1))
+            else:
+                setattr(data, i[1]+"_edge_attr", dist)
+
+        return data
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}(norm={self.norm}, '
+                f'max_value={self.max})')
